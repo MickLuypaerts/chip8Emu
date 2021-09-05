f4cf4ec690a02b7c7c4a34241360d192041cdd6c
Mick Luypaerts <mickluypaerts@gmail.com>
2021-09-05 13:29:32 +0200

    added String method to OpcodeInfo and aded opcode, programcount field

--- emulator/opcodeinfo.go
+++ emulator/opcodeinfo.go
@@ -1,17 +1,32 @@
 package emulator
 
+import "fmt"
+
 type OpcodeInfo struct {
-	opcodeName string
-	opcodeType string
-	opcodeDesc string
+	opcode       uint16
+	opcodeName   string
+	opcodeType   string
+	opcodeDesc   string
+	programCount uint16
 }
 
-func CreateOpcodeInfo(n string, t string, d string) OpcodeInfo {
-	var o OpcodeInfo
-	o.opcodeName = n
-	o.opcodeType = t
-	o.opcodeDesc = d
-	return o
+func (o *OpcodeInfo) String() string {
+	progStats := fmt.Sprintf("OPCODE: 0x%04X\n", o.opcode) +
+		fmt.Sprintf("Name:     %s\n", o.opcodeName) +
+		fmt.Sprintf("Type: %s\n", o.opcodeType) +
+		fmt.Sprintf("Desc: %s\n", o.opcodeDesc) +
+		fmt.Sprintf("PC: %d\n", o.programCount)
+	return progStats
+}
+
+func CreateOpcodeInfo(o uint16, n string, t string, d string, pc uint16) OpcodeInfo {
+	var oInfo OpcodeInfo
+	oInfo.opcode = o
+	oInfo.opcodeName = n
+	oInfo.opcodeType = t
+	oInfo.opcodeDesc = d
+	oInfo.programCount = pc
+	return oInfo
 }
 
 func (o OpcodeInfo) OpcodeName() string {
