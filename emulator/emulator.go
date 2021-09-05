caa42f4a32be5e4a73853a8916c86ffd92901816
Mick Luypaerts <mickluypaerts@gmail.com>
2021-09-05 12:38:46 +0200

    added support to map quit key to close the program and ClearTerminal func to clear the terminal after use

--- emulator/emulator.go
+++ emulator/emulator.go
@@ -1,6 +1,11 @@
 package emulator
 
-import "os"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+)
 
 type Chip interface {
 	ChipIniter
@@ -25,18 +30,27 @@ type Emulator struct {
 	chip     Chip
 	tui      TUI
 	controls map[string]func()
+
+	quitKey string
+	quit    chan struct{}
 }
 
 func (emu *Emulator) Run() {
-
+	go func() {
+		<-emu.quit
+		ClearTerminal()
+		os.Exit(0)
+	}()
 }
 
 func usage(name string, c map[string]Control, t map[string]Control) {
 	// TODO: usage()
 }
 
-func CreateEmulator(args []string, c Chip, t TUI) (*Emulator, error) {
+func CreateEmulator(args []string, quitKey string, c Chip, t TUI) (*Emulator, error) {
 	e := new(Emulator)
+	e.quitKey = quitKey
+	e.quit = make(chan struct{})
 	if len(args) < 2 {
 		usage(args[0], c.ControlsMap(), t.ControlsMap())
 		os.Exit(0)
@@ -51,7 +65,7 @@ func CreateEmulator(args []string, c Chip, t TUI) (*Emulator, error) {
 	if err != nil {
 		return nil, err
 	}
-	e.controls, err = CreateKeyFuncMap(c.ControlsMap(), t.ControlsMap())
+	e.controls, err = CreateKeyFuncMap(c.ControlsMap(), t.ControlsMap(), quitKey, e.quit)
 	if err != nil {
 		return nil, err
 	}
@@ -59,7 +73,7 @@ func CreateEmulator(args []string, c Chip, t TUI) (*Emulator, error) {
 }
 
 // TODO: make private
-func CreateKeyFuncMap(chip map[string]Control, tui map[string]Control) (map[string]func(), error) {
+func CreateKeyFuncMap(chip map[string]Control, tui map[string]Control, quitKey string, quitChan chan struct{}) (map[string]func(), error) {
 	c := make(map[string]func())
 	for k, v := range chip {
 		if _, ok := c[k]; !ok {
@@ -75,6 +89,9 @@ func CreateKeyFuncMap(chip map[string]Control, tui map[string]Control) (map[stri
 			return nil, DoubleKeyAssigmentError{Key: k}
 		}
 	}
+	if _, ok := c[quitKey]; !ok {
+		c[quitKey] = func() { close(quitChan) }
+	}
 	return c, nil
 }
 
@@ -82,5 +99,20 @@ func CreateKeyFuncMap(chip map[string]Control, tui map[string]Control) (map[stri
 func ExecuteKeyFunction(m map[string]func(), k string) {
 	if f, ok := m[k]; ok {
 		f()
+	}
+}
+
+// TODO: make private
+func ClearTerminal() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else if runtime.GOOS == "linux" {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	} else {
+		log.Println("operating system not supported for clearing terminal")
 	}
 }
